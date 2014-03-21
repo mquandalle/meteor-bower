9af718c5154c11d382be75bb5ed15f58752f0d98
Maxime Quandalle <maxime.quandalle@gmail.com>
2014-03-21 16:58:37 +0100

    Simplify the Bower object construction Remove the `bowerCommandSync` function

--- plugin/bower.js
+++ plugin/bower.js
@@ -1,25 +1,17 @@
 var bower = Npm.require("bower");
-
 var bowerCommands = ["info", "install", "link", "list", "lookup", "prune",
-  "register", "search", "update", "uninstall"];
+                     "register", "search", "update", "uninstall"];
 
-var bowerCommandSync = function (command) {
-  return Meteor._wrapAsync(function() {
+Bower = {};
+
+// Wrap every asynchronus bower command with `Meteor._wrapAsync`
+_.forEach(bowerCommands, function (command) {
+  Bower[command] = Meteor._wrapAsync(function() {
     arguments = _.toArray(arguments);
     var callback = arguments.pop();
     bower.commands[command]
       .apply(this, arguments)
-      .on('end', function(res) {
-        callback(null, res);
-      })
-      .on('error', function(err) {
-        callback(err, null);
-      });
+      .on('end', function(res) { callback(null, res); })
+      .on('error', function(err) { callback(err, null); });
   });
-};
-
-Bower = new Object();
-
-_.forEach(bowerCommands, function (command) {
-  Bower[command] = bowerCommandSync(command);
 });
