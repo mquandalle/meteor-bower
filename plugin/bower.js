3bb30bafaa1e1eb2cd9c52e93393486a265d099e
Maxime Quandalle <maxime.quandalle@gmail.com>
2014-04-03 19:36:13 +0200

    JShint: Don't re-use `arguments` as a variable name

--- plugin/bower.js
+++ plugin/bower.js
@@ -7,10 +7,10 @@ Bower = {};
 // Wrap every asynchronus bower command with `Meteor._wrapAsync`
 _.forEach(bowerCommands, function (command) {
   Bower[command] = Meteor._wrapAsync(function() {
-    arguments = _.toArray(arguments);
-    var callback = arguments.pop();
+    argsArray = _.toArray(arguments);
+    var callback = argsArray.pop();
     bower.commands[command]
-      .apply(this, arguments)
+      .apply(this, argsArray)
       .on('end', function(res) { callback(null, res); })
       .on('error', function(err) { callback(err, null); });
   });
