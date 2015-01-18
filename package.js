a4c23794c8ce2970b683af9cfba433f68b4fb37b
Maxime Quandalle <maxime@quandalle.com>
2015-01-18 01:56:34 +0100

    Upgrade the package version
    We now use a version number that matches the underlined bower version
    thanks the "new" `_` operator of meteor package system.

--- package.js
+++ package.js
@@ -2,7 +2,10 @@ Package.describe({
   summary: "Use Bower packages in your Meteor app",
   name: "mquandalle:bower",
   git: "https://github.com/mquandalle/meteor-bower.git",
-  version: "0.1.12"
+  // This matches the upstream version. If you want to publish a new version of
+  // the package without pulling a new upstream version, you should call it
+  // '1.3.12_1'.
+  version: "1.3.12"
 });
 
 Package.registerBuildPlugin({
