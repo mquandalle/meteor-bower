4cc0a369302a49be0a62dfd96bb6f4ccb0d9a208
Loren Sands-Ramshaw <lorensr@gmail.com>
2015-05-05 01:30:09 -0700

    bower v1.4.1

--- package.js
+++ package.js
@@ -4,8 +4,8 @@ Package.describe({
   git: "https://github.com/mquandalle/meteor-bower.git",
   // This matches the upstream version. If you want to publish a new version of
   // the package without pulling a new upstream version, you should call it
-  // '1.3.12_2'
-  version: "1.3.12_3"
+  // '1.4.1_1'
+  version: "1.4.1"
 });
 
 Package.registerBuildPlugin({
@@ -19,7 +19,7 @@ Package.registerBuildPlugin({
     "plugin/handler.js"
   ],
   npmDependencies: {
-    "bower": "1.3.12",
+    "bower": "1.4.1",
     "glob": "3.2.9"
   }
 });
